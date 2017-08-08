41cfa908234ff88e8cd1e075e1e172637737ad8e
Alisha Ukani <alishaaukani@gmail.com>
2017-08-08 18:33:34 -0400

    Display histograms for various audio features with pandas

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pprint import PrettyPrinter
 import spotipy
 import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 import secret # get API keys
 
@@ -14,7 +17,7 @@ pp = PrettyPrinter(indent=2)
 genre = "indie" # for initial testing. TODO: test multiple genres
 
 # get a genre's top artists
-top_artists_params = {'method': 'tag.gettopartists', 'tag': genre, 'limit': 1, 'api_key': secret.LF_KEY, 'format': 'json'}
+top_artists_params = {'method': 'tag.gettopartists', 'tag': genre, 'limit': 10, 'api_key': secret.LF_KEY, 'format': 'json'}
 top_artists_request = requests.get(LF_BASE, params=top_artists_params).json()
 top_artists = []
 for artist in top_artists_request['topartists']['artist']:
@@ -26,6 +29,7 @@ sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 # for each artist, get top tracks from Spotify
 top_tracks = []
+genre_audio_features = {}
 for artist in top_artists:
   artist_id = sp.search(artist, type="artist")["artists"]["items"][0]["id"]
   tracks = sp.artist_top_tracks(artist_id)["tracks"]
@@ -34,8 +38,6 @@ for artist in top_artists:
 
 # get audio features for each track
 audio_features = sp.audio_features(top_tracks)
-pp.pprint(audio_features)
-
 danceability = []
 energy = []
 speechiness = []
@@ -45,3 +47,17 @@ for song in audio_features:
   energy.append(song["energy"])
   speechiness.append(song["speechiness"])
   tempo.append(song["tempo"])
+
+# create pandas DataFrames
+danceability_obj = pd.DataFrame({'danceability' : pd.Series(danceability, index=top_tracks)})
+energy_obj = pd.DataFrame({'energy' : pd.Series(energy, index=top_tracks)})
+speechiness_obj = pd.DataFrame({'speechiness'  : pd.Series(speechiness, index=top_tracks)})
+tempo_obj = pd.DataFrame({'tempo' : pd.Series(tempo, index=top_tracks)})
+
+# display histograms
+plt.figure()
+danceability_obj.plot.hist()
+energy_obj.plot.hist()
+speechiness_obj.plot.hist()
+tempo_obj.plot.hist()
+plt.show(block=True)
