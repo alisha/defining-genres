eb9116690424c1a9d258b5fc2c2abda5eacde059
Alisha Ukani <alishaaukani@gmail.com>
2017-08-08 19:00:47 -0400

    Compare one feature for multiple genres

--- main.py
+++ main.py
@@ -14,42 +14,58 @@ LF_BASE = 'https://ws.audioscrobbler.com/2.0/'
 
 pp = PrettyPrinter(indent=2)
 
-genre = "indie" # for initial testing. TODO: test multiple genres
+danceability = {}
+energy = {}
+speechiness = {}
+tempo = {}
 
-# get a genre's top artists
-top_artists_params = {'method': 'tag.gettopartists', 'tag': genre, 'limit': 10, 'api_key': secret.LF_KEY, 'format': 'json'}
-top_artists_request = requests.get(LF_BASE, params=top_artists_params).json()
-top_artists = []
-for artist in top_artists_request['topartists']['artist']:
-  top_artists.append(artist['name'])
+genres = ["indie", "pop", "rap", "country"]
 
-# authenticate spotify
-client_credentials_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+for genre in genres:
+  # get a genre's top artists
+  top_artists_params = {'method': 'tag.gettopartists', 'tag': genre, 'limit': 1, 'api_key': secret.LF_KEY, 'format': 'json'}
+  top_artists_request = requests.get(LF_BASE, params=top_artists_params).json()
+  top_artists = []
+  for artist in top_artists_request['topartists']['artist']:
+    top_artists.append(artist['name'])
 
-# for each artist, get top tracks from Spotify
-top_tracks = []
-genre_audio_features = {}
-for artist in top_artists:
-  artist_id = sp.search(artist, type="artist")["artists"]["items"][0]["id"]
-  tracks = sp.artist_top_tracks(artist_id)["tracks"]
-  for track in tracks:
-    top_tracks.append(track["id"])
+  # authenticate spotify
+  client_credentials_manager = SpotifyClientCredentials()
+  sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-# get audio features for each track
-audio_features = sp.audio_features(top_tracks)
-danceability = []
-energy = []
-speechiness = []
-tempo = []
-for song in audio_features:
-  danceability.append(song["danceability"])
-  energy.append(song["energy"])
-  speechiness.append(song["speechiness"])
-  tempo.append(song["tempo"])
+  # for each artist, get top tracks from Spotify
+  top_tracks = []
+  for artist in top_artists:
+    artist_id = sp.search(artist, type="artist")["artists"]["items"][0]["id"]
+    tracks = sp.artist_top_tracks(artist_id)["tracks"]
+    for track in tracks:
+      top_tracks.append(track["id"])
+
+  # get audio features for each track
+  audio_features = sp.audio_features(top_tracks)
+  genre_danceability = []
+  genre_energy = []
+  genre_speechiness = []
+  genre_tempo = []
+  for song in audio_features:
+    genre_danceability.append(song["danceability"])
+    genre_energy.append(song["energy"])
+    genre_speechiness.append(song["speechiness"])
+    genre_tempo.append(song["tempo"])
+
+  # add genre's audio features to main feature dicts
+  danceability[genre] = pd.Series(genre_danceability)
+  energy[genre] = genre_energy
+  speechiness[genre] = genre_speechiness
+  tempo[genre] = genre_tempo
+
+danceability_obj = pd.DataFrame(danceability)
+plt.figure()
+danceability_obj.plot.hist(alpha=0.5, title="Danceability of hit songs for various genres")
+plt.show(block=True)
 
 # create pandas DataFrames
-danceability_obj = pd.DataFrame({'danceability' : pd.Series(danceability, index=top_tracks)})
+'''danceability_obj = pd.DataFrame(danceability)
 energy_obj = pd.DataFrame({'energy' : pd.Series(energy, index=top_tracks)})
 speechiness_obj = pd.DataFrame({'speechiness'  : pd.Series(speechiness, index=top_tracks)})
 tempo_obj = pd.DataFrame({'tempo' : pd.Series(tempo, index=top_tracks)})
@@ -60,4 +76,4 @@ danceability_obj.plot.hist()
 energy_obj.plot.hist()
 speechiness_obj.plot.hist()
 tempo_obj.plot.hist()
-plt.show(block=True)
+plt.show(block=True)'''
